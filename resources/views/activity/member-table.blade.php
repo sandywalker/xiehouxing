6d0e2ac768b61de6382ffaf7bb40d3c968c8bacb
sandywalker <sanddywalker@gmail.com>
2015-12-03 11:04:32 +0800

    optimize the order layout

--- resources/views/activity/member-table.blade.php
+++ resources/views/activity/member-table.blade.php
@@ -8,12 +8,13 @@
 			<th width="80">称呼</th>
 			<th width="100">联系电话</th>
 			@endif
-			<th width="80">性别</th>
-			<th width="80">人数</th>
-			<th>同行伙伴</th>
-			<th width="80">状态</th>
+			<th width="60">性别</th>
+			<th width="60">人数</th>
+			<th width="120">同行伙伴</th>
+			
 			@if($user->isme())
-			<th>操作</th>
+			<th width="70">状态</th>
+			<th width="120" class="text-center">操作</th>
 			@endif
 		</thead>
 		<tbody>
@@ -28,14 +29,16 @@
 			<td>@include('wedgits.sex',['sex'=>$member->sex])</td>
 			<td>{{$member->count}}</td>
 			<td>{{$member->companion}}</td>
-			<td>@include('activity.states',['states'=>$member->states])</td>
+			
 			@if($user->isme())
+			<td>@include('activity.states',['states'=>$member->states])</td>
 			<td>
+
 				@if ($member->states == 0)
-					<a href="/activities/{{$member->activity->id}}/orders/create?memberId={{$member->id}}" class="btn btn-warning btn-sm">下订单</a> &nbsp;&nbsp;
+					<a href="/activities/{{$member->activity->id}}/orders/create?memberId={{$member->id}}" class="btn btn-warning btn-xs">下订单</a> &nbsp;&nbsp;
 					<a  class="btn-remove" href="/activities/{{$activity->id}}/cancel/{{$member->id}}" data-message="机会难得，您确定取消此次活动报名吗？">取消</a>
 				@elseif($member->states > 0)
-					<a href="/activities/orders/{{$member->order->id}}" class="btn btn-success btn-sm" target="_blank">查看订单</a>
+					<a href="/activities/orders/{{$member->order->id}}" class="btn btn-success btn-xs" target="_blank">查看订单</a>
 				@endif
 			</td>
 			@endif
